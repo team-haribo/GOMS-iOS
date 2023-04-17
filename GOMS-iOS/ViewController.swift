42fa11c298c4786c62c0a3bb7e70f148c2b6af00
minaje0917 <k050918@naver.com>
2023-04-17 16:18:24 +0900

    :tada: Start GOMS

---
+++ GOMS-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GOMS-iOS
+//
+//  Created by 선민재 on 2023/04/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
