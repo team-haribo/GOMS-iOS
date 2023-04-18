7fc82ff96885874df475a523318d05dbf314fd1e
minaje0917 <k050918@naver.com>
2023-04-18 11:53:46 +0900

    :sparkles: Add :: Home features

---
+++ GOMS-iOS/Sources/Presentation/Main/Home/View/HomeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  HomeViewController.swift
+//  GOMS-iOS
+//
+//  Created by 선민재 on 2023/04/18.
+//
+
+import Foundation
+
+class HomeViewController: BaseViewController<HomeViewModel> {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func addView() {
+        <#code#>
+    }
+    
+    override func setLayout() {
+        <#code#>
+    }
+}
