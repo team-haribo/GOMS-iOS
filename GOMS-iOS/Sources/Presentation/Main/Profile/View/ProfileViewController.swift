2423223e2013df21c253f332cb01e7b1a7308a5a
minaje0917 <k050918@naver.com>
2023-04-21 02:01:34 +0900

    :sparkles: Add :: Profile

---
+++ GOMS-iOS/Sources/Presentation/Main/Profile/View/ProfileViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileViewController.swift
+//  GOMS-iOS
+//
+//  Created by 선민재 on 2023/04/21.
+//
+
+import UIKit
+
+class ProfileViewController: BaseViewController<BaseViewModel> {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func addView() {
+        <#code#>
+    }
+    
+    override func setLayout() {
+        <#code#>
+    }
+}
---
+++ GOMS-iOS/Sources/Presentation/Main/Profile/ViewModel/ProfileViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileViewModel.swift
+//  GOMS-iOS
+//
+//  Created by 선민재 on 2023/04/21.
+//
+
+import Foundation
+
+class ProfileViewModel: BaseViewModel {
+    
+}
