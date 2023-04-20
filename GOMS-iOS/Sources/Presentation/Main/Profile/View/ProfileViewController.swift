fe0466bd85a78ea111f51ae3d44345848e94ff31
minaje0917 <k050918@naver.com>
2023-04-21 08:54:23 +0900

    :sparkles: Add :: Profile user

--- GOMS-iOS/Sources/Presentation/Main/Profile/View/ProfileViewController.swift
+++ GOMS-iOS/Sources/Presentation/Main/Profile/View/ProfileViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Then
+import SnapKit
 
 class ProfileViewController: BaseViewController<BaseViewModel> {
 
@@ -13,11 +15,40 @@ class ProfileViewController: BaseViewController<BaseViewModel> {
         super.viewDidLoad()
     }
     
+    private let profileImage = UIImageView().then {
+        $0.image = UIImage(named: "profileImg")
+    }
+    
+    private let userName = UILabel().then {
+        $0.text = "선민재"
+        $0.font = UIFont.GOMSFont(size: 18,family: .Medium)
+        $0.textColor = .black
+    }
+    
+    private let userNum = UILabel().then {
+        $0.text = "3111"
+        $0.font = UIFont.GOMSFont(size: 14,family: .Regular)
+        $0.textColor = .subColor
+    }
+    
     override func addView() {
-        <#code#>
+        [profileImage, userName, userNum].forEach {
+            view.addSubview($0)
+        }
     }
     
     override func setLayout() {
-        <#code#>
+        profileImage.snp.makeConstraints {
+            $0.top.equalToSuperview().offset((bounds.height) / 7.31)
+            $0.centerX.equalToSuperview()
+        }
+        userName.snp.makeConstraints {
+            $0.top.equalTo(profileImage.snp.bottom).offset(6)
+            $0.centerX.equalToSuperview()
+        }
+        userNum.snp.makeConstraints {
+            $0.top.equalTo(userName.snp.bottom).offset(0)
+            $0.centerX.equalToSuperview()
+        }
     }
 }
