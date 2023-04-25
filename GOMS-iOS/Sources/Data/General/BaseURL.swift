4d1d58f32b8abfb52e13fea212828f72e00aba52
minaje0917 <k050918@naver.com>
2023-04-25 16:17:46 +0900

    :sparkles: Add:: General BaseURL

---
+++ GOMS-iOS/Sources/Data/General/BaseURL.swift
@@ -0,0 +1,13 @@
+//
+//  BaseURL.swift
+//  GOMS-iOS
+//
+//  Created by 선민재 on 2023/04/25.
+//
+
+import Foundation
+
+struct BaseURL {
+    static let baseURL = "https://port-0-goms-backend-nx562olfamh7iw.sel3.cloudtype.app/api/v1"
+    static let adminBaseURL = "\(baseURL)/admin"
+}
