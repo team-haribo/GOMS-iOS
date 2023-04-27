c3d30953b2db240298f7fb4c306b02fba532a2b0
minaje0917 <k050918@naver.com>
2023-04-27 11:52:17 +0900

    :sparkles: Add :: gauthSignInCompleted

--- GOMS-iOS/Sources/Presentation/Auth/Intro/View/IntroViewController.swift
+++ GOMS-iOS/Sources/Presentation/Auth/Intro/View/IntroViewController.swift
@@ -52,7 +52,7 @@ class IntroViewController: BaseViewController<IntroViewModel> {
         )
     }
     
-    private let gauthSignInButton = GAuthButton(auth: .continue, color: .colored, rounded: .default)
+    private let gauthSignInButton = GAuthButton(auth: .signin, color: .colored, rounded: .default)
     
     private func gauthButtonSetUp() {
         gauthSignInButton.prepare(
@@ -63,6 +63,7 @@ class IntroViewController: BaseViewController<IntroViewModel> {
             print("___________________________")
             print(code)
             print("___________________________")
+            self.viewModel.gauthSignInCompleted(code: code)
         }
     }
     
