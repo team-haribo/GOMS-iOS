0a33300bf640460644bf6366846b9a528688b328
minaje0917 <k050918@naver.com>
2023-04-27 11:04:49 +0900

    :sparkles: Add :: gauthSignInCompleted

--- GOMS-iOS/Sources/Presentation/Auth/Intro/ViewModel/IntroViewModel.swift
+++ GOMS-iOS/Sources/Presentation/Auth/Intro/ViewModel/IntroViewModel.swift
@@ -7,7 +7,45 @@
 
 import Foundation
 import RxFlow
+import RxCocoa
+import RxSwift
+import Moya
 
 class IntroViewModel: BaseViewModel, Stepper{
     
+    private let authProvider = MoyaProvider<AuthServices>(plugins: [NetworkLoggerPlugin()])
+    private var userData: SignInModel!
+    
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+    func transVC(input: Input) {
+    }
+
+}
+
+extension IntroViewModel {
+    func gauthSignInCompleted(code: String) {
+        let param = SignInRequest(code: code)
+        authProvider.request(.signIn(param: param)) { response in
+            switch response {
+            case let .success(result):
+                do {
+                    self.userData = try result.map(SignInModel.self)
+                    print(self.userData ?? "error")
+                    self.steps.accept(GOMSStep.tabBarIsRequired)
+                }catch(let err) {
+                    print(err.localizedDescription)
+                }
+                
+            case .failure(let err):
+                print(err.localizedDescription)
+            }
+        }
+    }
 }
