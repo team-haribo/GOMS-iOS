73dea3059ad6d034894c00a7156c22811c411f66
minaje0917 <k050918@naver.com>
2023-05-01 11:07:21 +0900

    :sparkles: OutingList API

--- GOMS-iOS/Sources/Presentation/Main/Outing/ViewModel/OutingViewModel.swift
+++ GOMS-iOS/Sources/Presentation/Main/Outing/ViewModel/OutingViewModel.swift
@@ -9,8 +9,12 @@ import Foundation
 import RxFlow
 import RxSwift
 import RxCocoa
+import Moya
 
 class OutingViewModel: BaseViewModel, Stepper{
+    private let outingProvider = MoyaProvider<OutingServices>(plugins: [NetworkLoggerPlugin()])
+    private var userData: OutingListResponse!
+    
     struct Input {
         let profileButtonTap: Observable<Void>
     }
@@ -27,5 +31,35 @@ class OutingViewModel: BaseViewModel, Stepper{
     
     private func pushProfileVC() {
         self.steps.accept(GOMSStep.profileIsRequired)
+    }
+}
+
+
+extension OutingViewModel {
+    func userAccountCompleted(Authorization: String) {
+        outingProvider.request(.outingList(authorization: Authorization)) { response in
+            switch response {
+            case let .success(result):
+                let responseData = result.data
+                do {
+                    self.userData = try JSONDecoder().decode(OutingListResponse.self, from: responseData)
+                }catch(let err) {
+                    print(String(describing: err))
+                }
+                let statusCode = result.statusCode
+                switch statusCode{
+                case 200..<300:
+                    self.steps.accept(GOMSStep.tabBarIsRequired)
+                case 401:
+                    
+                case 404:
+                    
+                default:
+                    print("ERROR")
+                }
+            case .failure(let err):
+                print(String(describing: err))
+            }
+        }
     }
 }
