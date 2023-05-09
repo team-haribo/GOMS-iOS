ea2f53fef9662cd6e445d2a0ee1e072b3afd14ce
minaje0917 <k050918@naver.com>
2023-05-09 16:14:27 +0900

    :sparkles: Add :: Onboarding Alert

--- GOMS-iOS/Sources/Application/Flow/Flows/Auth/AuthFlow.swift
+++ GOMS-iOS/Sources/Application/Flow/Flows/Auth/AuthFlow.swift
@@ -26,8 +26,16 @@ class AuthFlow: Flow {
         switch step {
         case .introIsRequired:
             return coordinateToIntro()
+            
         case .tabBarIsRequired:
             return .end(forwardToParentFlowWithStep: GOMSStep.tabBarIsRequired)
+            
+        case let .alert(title, message, style, actions):
+            return presentToAlert(title: title, message: message, style: style, actions: actions)
+            
+        case let .failureAlert(title, message, action):
+            return presentToFailureAlert(title: title, message: message, action: action)
+            
         default:
             return .none
         }
@@ -38,5 +46,23 @@ class AuthFlow: Flow {
         let vc = IntroViewController(vm)
         self.rootViewController.setViewControllers([vc], animated: false)
         return .one(flowContributor: .contribute(withNextPresentable: vc, withNextStepper: vm))
+    }
+    
+    func presentToAlert(title: String?, message: String?, style: UIAlertController.Style, actions: [UIAlertAction]) -> FlowContributors {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: style)
+        actions.forEach { alert.addAction($0) }
+        self.rootViewController.topViewController?.present(alert, animated: true)
+        return .none
+    }
+    
+    func presentToFailureAlert(title: String?, message: String?, action: [UIAlertAction]) -> FlowContributors {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        if !action.isEmpty {
+            action.forEach(alert.addAction(_:))
+        } else {
+            alert.addAction(.init(title: "확인", style: .default))
+        }
+        self.rootVC.topViewController?.present(alert, animated: true)
+        return .none
     }
 }
