c3f21e7f27eccb3fea4b4f1bdb4023bd76a06e50
minaje0917 <k050918@naver.com>
2023-05-11 15:38:49 +0900

    :lipstick: Add :: StudentInfo View,ViewModel Setting

---
+++ GOMS-iOS/Sources/Presentation/Main/StudentInfo/View/StudentInfoViewController.swift
@@ -0,0 +1,16 @@
+//
+//  StudentInfoViewController.swift
+//  GOMS-iOS
+//
+//  Created by 선민재 on 2023/05/11.
+//
+
+import UIKit
+
+class StudentInfoViewController: BaseViewController<StudentInfoViewModel> {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ GOMS-iOS/Sources/Presentation/Main/StudentInfo/ViewModel/StudentInfoViewModel.swift
@@ -0,0 +1,20 @@
+import Foundation
+import RxFlow
+import RxCocoa
+import RxSwift
+import Moya
+
+class StudentInfoViewModel: BaseViewModel, Stepper{
+    
+    struct Input {
+        let logoutButtonDidTap: Observable<Void>
+    }
+    
+    struct Output {
+        
+    }
+    
+    func transVC(input: Input) {
+        
+    }
+}
