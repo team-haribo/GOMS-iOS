11c722fb0a5a620feada109f23fccf9f4def6206
minaje0917 <k050918@naver.com>
2023-05-11 11:01:11 +0900

    :ambulance: HotFix :: Alert 안 뜨는 버그 수정

--- GOMS-iOS/Sources/Application/Flow/Flows/Main/Items/QRCodeFlow.swift
+++ GOMS-iOS/Sources/Application/Flow/Flows/Main/Items/QRCodeFlow.swift
@@ -46,6 +46,12 @@ class QRCodeFlow: Flow {
         case .introIsRequired:
             return .end(forwardToParentFlowWithStep: GOMSStep.introIsRequired)
             
+        case let .alert(title, message, style, actions):
+            return presentToAlert(title: title, message: message, style: style, actions: actions)
+            
+        case let .failureAlert(title, message, action):
+            return presentToFailureAlert(title: title, message: message, action: action)
+            
         default:
             return .none
         }
@@ -56,5 +62,23 @@ class QRCodeFlow: Flow {
         let vc = QRCodeViewController(vm)
         self.rootViewController.pushViewController(vc, animated: true)
         return .one(flowContributor: .contribute(withNextPresentable: vc, withNextStepper: vm))
+    }
+    
+    func presentToAlert(title: String?, message: String?, style: UIAlertController.Style, actions: [UIAlertAction]) -> FlowContributors {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: style)
+        actions.forEach { alert.addAction($0) }
+        self.rootViewController.topViewController?.present(alert, animated: true)
+        return .none
+    }
+    
+    func presentToFailureAlert(title: String?, message: String?, action: [UIAlertAction]) -> FlowContributors {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        if !action.isEmpty {
+            action.forEach(alert.addAction(_:))
+        } else {
+            alert.addAction(.init(title: "확인", style: .default))
+        }
+        self.rootViewController.topViewController?.present(alert, animated: true)
+        return .none
     }
 }
