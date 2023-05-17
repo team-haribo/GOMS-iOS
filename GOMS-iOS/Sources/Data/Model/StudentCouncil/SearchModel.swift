3af264760d4ca221f88e3b6ade024dffbfdb5d46
minaje0917 <k050918@naver.com>
2023-05-17 16:56:22 +0900

    :sparkles: Add :: SearchModel

---
+++ GOMS-iOS/Sources/Data/Model/StudentCouncil/SearchModel.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+struct SearchModel: Codable {
+    let data: StudentInfoResponse
+}
+
+struct SearchResponse: Codable {
+    let accountIdx: UUID
+    let name: String
+    let studentNum: StudentNum
+    let profileUrl: String?
+    let authority: String
+    let isBlackList: Bool
+    
+    struct StudentNum: Codable{
+        let grade: Int
+        let classNum: Int
+        let number: Int
+    }
+}
