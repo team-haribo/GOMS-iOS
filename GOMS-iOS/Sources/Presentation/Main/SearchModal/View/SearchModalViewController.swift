dee9ee62474ff0878f3b7427afdab63c11da35ba
minaje0917 <k050918@naver.com>
2023-05-17 15:46:27 +0900

    :sparkles: Add :: SearchModal View, ViewModel

---
+++ GOMS-iOS/Sources/Presentation/Main/SearchModal/View/SearchModalViewController.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+class SearchModalViewController: BaseViewController<SearchModalViewModal> {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func addView() {
+        
+    }
+    
+    override func setLayout() {
+        
+    }
+}
---
+++ GOMS-iOS/Sources/Presentation/Main/SearchModal/ViewModel/SearchModalViewModel.swift
@@ -0,0 +1,18 @@
+import Foundation
+import RxFlow
+import RxCocoa
+
+class SearchModalViewModal: BaseViewModel, Stepper{
+    
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+    func transVC(input: Input) {
+        
+    }
+}
