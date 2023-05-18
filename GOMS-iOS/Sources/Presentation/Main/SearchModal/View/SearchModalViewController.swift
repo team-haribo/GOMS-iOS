610c27c72d99f786414b9b70aee951d3d4424065
minaje0917 <k050918@naver.com>
2023-05-18 17:06:44 +0900

    :fire: Remove :: RoleCollectionView

--- GOMS-iOS/Sources/Presentation/Main/SearchModal/View/SearchModalViewController.swift
+++ GOMS-iOS/Sources/Presentation/Main/SearchModal/View/SearchModalViewController.swift
@@ -8,12 +8,6 @@ class SearchModalViewController: BaseViewController<SearchModalViewModal> {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        roleCollectionView.dataSource = self
-        roleCollectionView.delegate = self
-        roleCollectionView.register(
-            RoleCell.self,
-            forCellWithReuseIdentifier: RoleCell.identifier
-        )
     }
     
     private var searchBar = UITextField().then {
@@ -50,16 +44,8 @@ class SearchModalViewController: BaseViewController<SearchModalViewModal> {
         $0.textColor = .black
     }
     
-    private let roleCollectionView = UICollectionView(
-        frame: .zero,
-        collectionViewLayout: UICollectionViewFlowLayout()
-    ).then {
-        $0.isScrollEnabled = false
-        $0.backgroundColor = .background
-    }
-    
     override func addView() {
-        [searchBar,roleText,gradeText,classNumText,roleCollectionView].forEach {
+        [searchBar,roleText,gradeText,classNumText].forEach {
             view.addSubview($0)
         }
     }
@@ -86,43 +72,5 @@ class SearchModalViewController: BaseViewController<SearchModalViewModal> {
             $0.top.equalTo(gradeText.snp.bottom).offset(65)
             $0.leading.equalToSuperview().offset(26)
         }
-        
-        roleCollectionView.snp.makeConstraints {
-            $0.top.equalTo(roleText.snp.bottom).offset(6)
-            $0.leading.equalToSuperview().offset(26)
-            $0.trailing.equalToSuperview().inset(75)
-            $0.bottom.equalTo(gradeText.snp.bottom).inset(40)
-        }
-    }
-}
-extension SearchModalViewController :
-    UICollectionViewDelegate,
-    UICollectionViewDelegateFlowLayout,
-    UICollectionViewDataSource {
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 3
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let roleCell = collectionView.dequeueReusableCell(withReuseIdentifier: RoleCell.identifier, for: indexPath) as? RoleCell else {
-            return UICollectionViewCell()
-        }
-        roleCell.backgroundColor = .white
-        roleCell.layer.cornerRadius = 8
-        roleCell.layer.applySketchShadow(
-            color: UIColor.black,
-            alpha: 0.1,
-            x: 0,
-            y: 2,
-            blur: 8,
-            spread: 0
-        )
-        roleCell.roleText.text = roleName[indexPath.row]
-        return roleCell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 74, height: 34)
     }
 }
