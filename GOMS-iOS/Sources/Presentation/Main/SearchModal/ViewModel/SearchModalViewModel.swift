27bd2ae240dd844d23c7e090c1d49cd589832cdf
minaje0917 <k050918@naver.com>
2023-05-21 13:55:21 +0900

    :sparkles: Add :: SearchAPI In SearchViewModel

--- GOMS-iOS/Sources/Presentation/Main/SearchModal/ViewModel/SearchModalViewModel.swift
+++ GOMS-iOS/Sources/Presentation/Main/SearchModal/ViewModel/SearchModalViewModel.swift
@@ -18,3 +18,35 @@ class SearchModalViewModal: BaseViewModel, Stepper{
         
     }
 }
+extension SearchModalViewModal {
+    func searchStudent(completion: @escaping () -> Void, grade: Int?, classNum: Int?, name: String?, isBlackList: Bool?, authority: String?) {
+        studentCouncilProvider.request(.search(authorization: accessToken, grade: grade, classNum: classNum, name: name, isBlackList: isBlackList, authority: authority)){ response in
+            switch response {
+            case let .success(result):
+                let responseData = result.data
+                print(String(data: responseData, encoding: .utf8))
+                do {
+                    self.searchResult = try JSONDecoder().decode([SearchResponse].self, from: responseData)
+                    completion()
+                }catch(let err) {
+                    print(String(describing: err))
+                }
+                let statusCode = result.statusCode
+                switch statusCode{
+                case 200..<300:
+                    print("success")
+                case 401:
+                    self.gomsRefreshToken.tokenReissuance()
+                case 404:
+                    self.steps.accept(GOMSStep.failureAlert(title: "오류", message: "학생회 계정이 아닙니다."))
+                    self.steps.accept(GOMSStep.introIsRequired)
+                default:
+                    print("ERROR")
+                }
+            case .failure(let err):
+                print(String(describing: err))
+            }
+        }
+    }
+
+}
