d953a0cd1bf85417203c707de3f3b44fb6a30fc9
minaje0917 <k050918@naver.com>
2023-07-31 22:17:04 +0900

    :bug: Fix :: 라이브러리 추가

--- Project.swift
+++ Project.swift
@@ -10,7 +10,40 @@ let targetVersion = "15.0"
 let project = Project(
   name: projectName,
   organizationName: organizationName,
-  packages: [],
+  packages: [
+    .remote(
+        url: "https://github.com/RxSwiftCommunity/RxFlow.git",
+        requirement: .upToNextMajor(from: "2.10.0")
+    ),
+    .remote(
+        url: "https://github.com/ReactiveX/RxSwift.git",
+        requirement: .upToNextMajor(from: "6.6.0")
+    ),
+    .remote(
+        url: "https://github.com/SnapKit/SnapKit.git",
+        requirement: .upToNextMajor(from: "5.0.1")
+    ),
+    .remote(
+        url: "https://github.com/devxoul/Then",
+        requirement: .upToNextMajor(from: "2")
+    ),
+    .remote(
+        url: "https://github.com/Moya/Moya.git",
+        requirement: .upToNextMajor(from: "15.0.0")
+    ),
+    .remote(
+        url: "https://github.com/CocoaPods/Specs.git",
+        requirement: .upToNextMajor(from: "7.0")
+    ),
+    .remote(
+        url: "https://github.com/yannickl/QRCodeReader.swift.git",
+        requirement:.upToNextMajor(from: "10.1.0")
+    ),
+    .remote(
+        url: "https://github.com/GSM-MSG/GAuthSignin-Swift",
+        requirement: .upToNextMajor(from: "0.0.3")
+    )
+  ],
   settings: nil,
   targets: [
     Target(name: projectName,
@@ -21,7 +54,16 @@ let project = Project(
            infoPlist: .default,
            sources: ["\(projectName)/**"],
            resources: [],
-           dependencies: [] // tuist generate할 경우 pod install이 자동으로 실행
+           dependencies: [
+            .package(product: "RxFlow"),
+            .package(product: "RxSwift"),
+            .package(product: "RxCocoa"),
+            .package(product: "Then"),
+            .package(product: "Moya"),
+            .package(product: "Kingfisher"),
+            .package(product: "QRCodeReader"),
+            .package(product: "GAuthSignin")
+           ] // tuist generate할 경우 pod install이 자동으로 실행
           )
   ],
   schemes: [
