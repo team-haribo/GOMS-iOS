309717f6d4ae8ad7b2ccd804cf21ea00144f51a4
Shin ain <s22028@gsm.hs.kr>
2023-10-04 15:26:04 +0900

    :alien: Update :: baseURL

--- GOMS-iOS/Sources/Data/General/BaseURL.swift
+++ GOMS-iOS/Sources/Data/General/BaseURL.swift
@@ -1,5 +1,5 @@
 import Foundation
 
 struct BaseURL {
-    static let baseURL = "https://server.goms.site/api/v1"
+    static let baseURL = "https://port-0-goms-backend-12fhqa2bln49rbi0.sel5.cloudtype.app/api/v1"
 }
