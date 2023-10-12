b9cd91e1a762f26c93fcd61dd3c9e1195c5b9b5d
minaje0917 <k050918@naver.com>
2023-10-12 10:20:05 +0900

    :sparkles: Add :: UILabel + Rx

--- GOMS-iOS/Sources/Presentation/Utils/Extension/Rx/UILabel+Rx.swift
+++ GOMS-iOS/Sources/Presentation/Utils/Extension/Rx/UILabel+Rx.swift
@@ -1,9 +1,22 @@
-//
-//  UILabel+Rx.swift
-//  GOMS-iOS
-//
-//  Created by 선민재 on 10/11/23.
-//  Copyright © 2023 HARIBO. All rights reserved.
-//
+import RxSwift
+import RxCocoa
+import UIKit
 
-import Foundation
+extension Reactive where Base: UILabel {
+    var outingCount: Binder<Int> {
+        Binder(base) { label, outingCount in
+            label.text = "\(outingCount) 명이 외출중이에요!"
+            label.textColor = UIColor.black
+            label.font = UIFont.GOMSFont(size: 16,family: .Medium)
+            let fullText = label.text ?? ""
+            let attribtuedString = NSMutableAttributedString(string: fullText)
+            let range = (fullText as NSString).range(of: "\(outingCount)")
+            attribtuedString.addAttribute(
+                .foregroundColor,
+                value: UIColor.mainColor?.cgColor ?? UIColor.black,
+                range: range
+            )
+            label.attributedText = attribtuedString
+        }
+    }
+}
